5750de9b0813698693bb7d48a38dbfb383eaf57d
JyotirmayaMajhi <jyotirmayamajhi1998@gamil.com>
2021-11-21 16:54:16 +0530

    Added: Frequency of Words in large paragraph

--- src/com/bridgelabz/hashtable/Main.java
+++ src/com/bridgelabz/hashtable/Main.java
@@ -3,23 +3,23 @@ package com.bridgelabz.hashtable;
 public class Main {
 
 	public static void main(String[] args) {
-		String string = "To be or not to be";
+		String string = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 		String[] words = string.split(" ");
 
-		LinkedHashMap linked_Hash_Map = new LinkedHashMap();
+		LinkedHashMap linkedHashMap = new LinkedHashMap();
 
 		for (int i = 0; i < words.length; i++) {
 
 			String word = words[i];
-			Integer value = linked_Hash_Map.get(word);
+			Integer value = linkedHashMap.get(word);
 			if (value == null) {
 				value = 1;
 			} else {
 				value += 1;
 			}
-			linked_Hash_Map.put(word, value);
+			linkedHashMap.put(word, value);
 		}
-		System.out.println(linked_Hash_Map);
+		System.out.println(linkedHashMap);
 
 	}
 
